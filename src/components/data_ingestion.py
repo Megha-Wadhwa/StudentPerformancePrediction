140e8d6fd3549d4c93c56b31bfcc10c95ffa0565
Megha-Wadhwa <meghaw42@gmail.com>
2024-05-25 18:45:21 +0530

    Data Transformation completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,17 +7,21 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass #decorator
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', 'train.csv')
-    test_data_path: str = os.path.join('artifacts',"test.csv")
-    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+    train_data_path: str = os.path.join('artifacts/DataIngestion', 'train.csv')
+    test_data_path: str = os.path.join('artifacts/DataIngestion',"test.csv")
+    raw_data_path: str = os.path.join('artifacts/DataIngestion', 'data.csv')
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
-    def initate_data_ingestion(self):
+    def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or Component")
         try:
             df=pd.read_csv('notebook/data/stud.csv')
@@ -44,6 +48,11 @@ class DataIngestion:
             raise CustomException(e,sys)
             pass
 
+
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data , test_data)
+                
---
+++ src/utils.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import dill
+import pickle
+
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path, obj):
+    try:
+        logging.info("Saving preprocessing obj as pickle file")
+        dir_path= os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok = True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
